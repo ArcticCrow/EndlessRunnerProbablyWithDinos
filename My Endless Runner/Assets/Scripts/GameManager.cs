fd4d560468e705e0264b191d7cf7b4483be1cafa
ArcticCrow <arctic-crow@web.de>
2018-09-17 15:54:44 +0200

    GM, Player Control, Camera Control
    + Game Manager that manages speed
    + Player controls that allow player to move left and right and jump with controlled jumps
     > Jumps still need fixing
    + Made Camera Auto Scroll

---
+++ My Endless Runner/Assets/Scripts/GameManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    #region Public Variables
+    // Reference to the player game object
+    public GameObject player;
+    // Reference to the camera that is supposed to follow the player
+    public GameObject mainCamera;
+
+    // What is the base game speed
+    public float startingSpeed = 100f;
+    // By how much should the speed increase
+    public float acceleration = 1.01f;
+    // How often should the speed increase
+    public float accelerationInterval = 2f;
+    // How fast is the game currently running
+    public float currentSpeed = 0;
+    #endregion
+
+    #region Private Variables
+    // How long until the speed increases
+    private float nextIncrease = 0;
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+        if (mainCamera == null)
+        {
+            mainCamera = Camera.main.gameObject;
+        }
+        // Reset the increase timer
+        nextIncrease = accelerationInterval;
+
+        // Set the base running speed
+        currentSpeed = startingSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Check if player is still in bounds
+        Vector2 playerPos = new Vector2(player.transform.position.x, player.transform.position.y);
+        Vector2 playerHalfScale = new Vector2(player.transform.localScale.x, player.transform.localScale.y);
+        Bounds camBounds = OrthographicBounds(mainCamera.GetComponent<Camera>());
+        //Debug.Log(camBounds);
+
+        if (playerPos.x + playerHalfScale.x < camBounds.min.x || playerPos.x - playerHalfScale.x > camBounds.max.x
+            || playerPos.y + playerHalfScale.y < camBounds.min.y || playerPos.y - playerHalfScale.y > camBounds.max.y)
+        {
+            Debug.Log("Player is outside of bounds!");
+            // TODO game over
+        }
+
+        // calculate camera offset
+        float horCamOffset = Mathf.Lerp(0, currentSpeed * Time.deltaTime, Time.time);
+        mainCamera.transform.position += new Vector3 (horCamOffset, 0, 0);
+	}
+
+    private void FixedUpdate()
+    {
+        // Everytime the the interval has elapsed, increase the current game speed
+        //  and reset the timer
+        nextIncrease -= Time.fixedDeltaTime;
+        if (nextIncrease <= 0)
+        {
+            currentSpeed *= acceleration;           // accelerate
+            nextIncrease += accelerationInterval;   // reset timer
+        }
+        //Debug.Log("next increase in '" + nextIncrease + "'; current speed is " + currentSpeed);
+    }
+
+    // Calculating camera bounds
+    private Bounds OrthographicBounds(Camera camera)
+    {
+        float screenAspect = (float)Screen.width / (float)Screen.height;
+        float cameraHeight = camera.orthographicSize * 2;
+        Bounds bounds = new Bounds(
+            camera.transform.position,
+            new Vector3(cameraHeight * screenAspect, cameraHeight, 0));
+        return bounds;
+    }
+}
---
+++ My Endless Runner/Assets/Scripts/PlayerRunnerController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRunnerController : MonoBehaviour {
+
+    #region Public Variables
+    // Reference to GameManager to get GameSpeed
+    public GameManager gm;
+    // How fast the player can move left and right
+    public float moveSpeed = 3f;
+    // How long and forceful can the player jump
+    public float jumpForce = 7f;
+    public float jumpTime = 2f;
+    #endregion
+
+    #region private Variables
+    private Rigidbody2D rb;
+    
+    // Flag to determine if player is grounded
+    private bool isGrounded = false;
+
+    // Remaining time for jump acceleration
+    private float jumpTimeRemaining;
+    // Flag to determine if player is still holding down jump
+    private bool stoppedJumping = false;
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        jumpTimeRemaining = jumpTime;
+    }
+	
+	// Update is called once per frame
+	private void Update () {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        #region Player Movement
+        if (rb != null)
+        {
+            // Move player left and right
+            float horizontalSpeed = Input.GetAxis("Horizontal") * moveSpeed;
+            float verticalSpeed = rb.velocity.y;
+
+            // Control player jumping 
+            // FIXME multiple jumping allowed sometimes???
+            if (isGrounded)
+            {
+                if (Input.GetButton("Jump"))
+                {
+                    verticalSpeed = jumpForce;
+                    stoppedJumping = false;
+                }
+            }
+            // If player releases button, stop jumping
+            else if (Input.GetButtonUp("Jump"))
+            {
+                jumpTimeRemaining = 0;
+                stoppedJumping = true;
+            }
+            // Air control with more powerful jump if the player hold the button longer
+            //  and the player still has remainig jump time
+            else if (Input.GetButton("Jump") && !stoppedJumping && jumpTimeRemaining > 0)
+            {
+                verticalSpeed = jumpForce;
+                jumpTimeRemaining -= Time.deltaTime;
+            }
+
+            // apply player velocity
+            rb.velocity = new Vector2(gm.currentSpeed + horizontalSpeed, verticalSpeed);
+        }
+        #endregion
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Platform"))
+        {
+            foreach (ContactPoint2D contact in collision.contacts)
+            {
+                if (contact.normal.y > 0.5)
+                {
+                    isGrounded = true;
+                    jumpTimeRemaining = jumpTime;
+                    break;
+                }
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Platform"))
+        {
+            isGrounded = false;
+        }
+    }
+}
