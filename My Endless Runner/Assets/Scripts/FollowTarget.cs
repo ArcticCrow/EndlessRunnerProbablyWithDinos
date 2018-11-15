64e9eea65603af2555c62327fe6bfde74bc5e0c7
Daniel <arctic-crow@web.de>
2018-11-15 16:50:23 +0100

    Chaaracter Control, Animation and Camera Finished (for now)
    Character Controller needs more Documentation; enables player to move (run and walk) horizontally and jump at different power levels, speed limits are applied and animations updated accordingly, transitions are smooth but feel responsive (might need tweaking)
    
    Camera Controller/Follow Target Script is well documented; enables following a target/player smoothly with a given offset, thinking of adding zoom
    
    enables

---
+++ My Endless Runner/Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour {
+
+	#region Public Variables
+	[Tooltip("Should the game object look for a game object with the player tag automatically?")]
+	public bool followPlayer = false;
+	[Tooltip("The target transform the game object will follow. You may leave this empty if 'followPlayer' is ticked.")]
+	public Transform target;
+
+	[Header("Position Offsetting")]
+	public bool calculateOffsetAtStart = true;
+	[Tooltip("The positional offset towards the followed object. You may leave this empty if 'calcualteOffsetAtStart' is ticked.")]
+	public Vector3 offset;
+	[Tooltip("The distance that the gameobject should look ahead of the target in a direction when following.")]
+	public Vector2 lookAheadDistance = Vector2.zero;
+
+	[Header("Smoothing")]
+	[Tooltip("How fast should the game object snap to the targets position?")]
+	public float smoothingTime = 0.05f;
+	[Tooltip("How fast should the game object snap to the targets position?")]
+	public float maxTransitionSpeed = 1f;
+
+	[Tooltip("Should the game object snap back to the targets position at a different speed?")]
+	public bool useDifferentReturnSpeed = false;
+	public float returnTime = 0.01f;
+	#endregion
+
+
+	#region Private Variables
+	private Vector3 lastPos;
+	private Vector3 velocity = Vector3.zero;
+	private Vector3 lookAheadOffset;
+	#endregion
+
+
+	// Use this for initialization
+	void Start () {
+		if (followPlayer)
+		{
+			target = GameObject.FindGameObjectWithTag("Player").transform;
+		}
+		if (target == null)
+		{
+			throw new System.Exception("No target to follow was given or found!");
+		}
+
+		if (calculateOffsetAtStart)
+		{
+			offset = transform.position - target.position;
+			lastPos = target.position;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (target != null)
+		{
+			Vector3 targetPosition = target.position + offset;
+			if (!useDifferentReturnSpeed || lastPos != target.position)
+			{
+				// Calculate the look ahead offset (ignoring negative y values to avoid wonkiness)
+				// TODO smooth look ahead value to avoid massive leaps in position
+				lookAheadOffset = Vector3.Scale((target.position - lastPos), new Vector3(1, 1, 0));
+				lookAheadOffset.x *= lookAheadDistance.x;
+				lookAheadOffset.y *= (lookAheadOffset.y < 0) ? 0 : lookAheadDistance.y;
+				targetPosition += lookAheadOffset;
+
+				// Smoothly reposition to the new position using smoothing time and max speed
+				transform.position = Vector3.SmoothDamp(transform.position, targetPosition,
+					ref velocity, smoothingTime, maxTransitionSpeed);
+			}
+			else
+			{
+				// Smoothly reposition to the new position using return time
+				transform.position = Vector3.SmoothDamp(transform.position, targetPosition,
+					ref velocity, returnTime);
+			}
+
+			// Store targets last position for testing in next cycle
+			lastPos = target.position;
+		}
+	}
+}
--- My Endless Runner/Assets/Scripts/PlayerRunnerController.cs
+++ My Endless Runner/Assets/Scripts/PlayerRunnerController.cs
@@ -5,17 +5,35 @@ using UnityEngine;
 public class PlayerRunnerController : MonoBehaviour {
 
     #region Public Variables
-    // Reference to GameManager to get GameSpeed
-    public GameManager gm;
     // How fast the player can move left and right
-    public float moveSpeed = 3f;
+    public float horizontalAcceleration = 3f;
+	public float maxWalkSpeed = 3f;
+	public float maxRunSpeed = 5f;
+
     // How long and forceful can the player jump
-    public float jumpForce = 7f;
-    public float jumpTime = 2f;
+    public float jumpForce = 5f;
+    public float jumpControlTime = .2f;
+
+	public float maxJumpSpeed = 5f;
+
+
+	public enum AnimationState
+	{
+		Idle = 0,
+		Walking = 1,
+		Running = 2,
+		Jumping = 3,
+		Airborne = 4,
+	}
+
+	public AnimationState state = AnimationState.Idle;
     #endregion
 
     #region private Variables
     private Rigidbody2D rb;
+	private Animator anim;
+
+	private bool flipX = false;
     
     // Flag to determine if player is grounded
     private bool isGrounded = false;
@@ -24,68 +42,115 @@ public class PlayerRunnerController : MonoBehaviour {
     private float jumpTimeRemaining;
     // Flag to determine if player is still holding down jump
     private bool stoppedJumping = false;
+
+	private Vector3 scale;
     #endregion
 
     // Use this for initialization
     void Start () {
         rb = GetComponent<Rigidbody2D>();
-        jumpTimeRemaining = jumpTime;
+		anim = transform.GetComponentInChildren<Animator>();
+        jumpTimeRemaining = jumpControlTime;
+		scale = transform.localScale;
     }
 	
 	// Update is called once per frame
-	private void Update () {
-        
-    }
+	private void Update ()
+	{
+		#region Player Movement & Animation
+		if (rb != null)
+		{
+			float horizontalSpeed = Input.GetAxis("Horizontal") * horizontalAcceleration;
 
-    private void FixedUpdate()
-    {
-        #region Player Movement
-        if (rb != null)
-        {
-            // Move player left and right
-            float horizontalSpeed = Input.GetAxis("Horizontal") * moveSpeed;
-            float verticalSpeed = rb.velocity.y;
+			state = AnimationState.Idle;
 
-            // Control player jumping 
-            // FIXME multiple jumping allowed sometimes???
-            if (isGrounded)
-            {
-                if (Input.GetButton("Jump"))
-                {
-                    verticalSpeed = jumpForce;
-                    stoppedJumping = false;
-                }
-            }
-            // If player releases button, stop jumping
-            else if (Input.GetButtonUp("Jump"))
-            {
-                jumpTimeRemaining = 0;
-                stoppedJumping = true;
-            }
-            // Air control with more powerful jump if the player hold the button longer
-            //  and the player still has remainig jump time
-            else if (Input.GetButton("Jump") && !stoppedJumping && jumpTimeRemaining > 0)
-            {
-                verticalSpeed = jumpForce;
-                jumpTimeRemaining -= Time.deltaTime;
-            }
+			// Apply player velocity
+			// Reset velocity to avoid sliding upon direction change
+			if ((horizontalSpeed > 0 && rb.velocity.x < 0) || (horizontalSpeed < 0 && rb.velocity.x > 0))
+			{
+				rb.velocity = new Vector2(rb.velocity.x * 0.1f, rb.velocity.y);
+			}
+			rb.AddForce(new Vector2(horizontalSpeed, 0), ForceMode2D.Force);
 
-            // apply player velocity
-            rb.velocity = new Vector2(gm.currentSpeed + horizontalSpeed, verticalSpeed);
-        }
-        #endregion
-    }
+			// Limit velocity (horizontal movement)
+			float horMaxSpeed = (Input.GetButton("Walk")) ? maxWalkSpeed : maxRunSpeed;
+			Vector2 newLimitedVelocity = rb.velocity;
+			newLimitedVelocity.x = (Mathf.Abs(newLimitedVelocity.x) > horMaxSpeed) ? horMaxSpeed * Mathf.Sign(newLimitedVelocity.x) : newLimitedVelocity.x;
+			
+
+			if (newLimitedVelocity.x != 0)
+			{
+				if (Mathf.Abs(newLimitedVelocity.x) <= maxWalkSpeed)
+				{
+					state = AnimationState.Walking;
+				}
+				else
+				{
+					state = AnimationState.Running;
+				}
+			}
+
+			// Control player jumping 
+			if (isGrounded && Input.GetButton("Jump"))
+			{
+				stoppedJumping = false;
+				isGrounded = false;
+				rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+
+				state = AnimationState.Jumping;
+			}
+			else if (Input.GetButton("Jump") && !stoppedJumping && jumpTimeRemaining > 0)
+			{
+				// Air control with more powerful jump if the player holds the button longer
+				//  and the player still has remainig jump time
+				jumpTimeRemaining -= Time.deltaTime;
+				rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+
+				state = AnimationState.Jumping;
+			}
+			else if (!isGrounded)
+			{
+				// Player is airborne so disable jumping
+				jumpTimeRemaining = 0;
+				stoppedJumping = true;
+
+				state = AnimationState.Airborne;
+			}
+
+			// Flip character if moving negative direction
+			// Check if the player is walking and if the sprite needs to be mirrored
+			if (newLimitedVelocity.x > 0f && flipX)
+			{
+				flipX = false;
+				transform.localScale = scale;
+			}
+			else if (newLimitedVelocity.x < 0f && !flipX)
+			{
+				flipX = true;
+				transform.localScale = Vector3.Scale(scale, new Vector3(-1, 1, 1));
+			}
+
+			// Limit velocity (vertical movement)
+			newLimitedVelocity.y = rb.velocity.y;
+			newLimitedVelocity.y = (newLimitedVelocity.y > maxJumpSpeed) ? maxJumpSpeed : newLimitedVelocity.y;
+			rb.velocity = newLimitedVelocity;
+		}
+		#endregion
+
+		// Apply current state to animator
+		anim.SetInteger("state", (int)state);
+	}
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Platform"))
+		if (collision.gameObject.CompareTag("Platform"))
         {
             foreach (ContactPoint2D contact in collision.contacts)
             {
                 if (contact.normal.y > 0.5)
                 {
                     isGrounded = true;
-                    jumpTimeRemaining = jumpTime;
+                    jumpTimeRemaining = jumpControlTime;
                     break;
                 }
             }
